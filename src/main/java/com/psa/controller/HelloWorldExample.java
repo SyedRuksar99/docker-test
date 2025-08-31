c1eee54332e15add604a7f8424c175928444db8d
ruksar <ruksarbanu.gm@gmail.com>
2025-08-31 20:29:03 +0530

    demo-app-v1

---
+++ src/main/java/com/psa/controller/HelloWorldExample.java
@@ -0,0 +1,15 @@
+package com.psa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldExample {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        System.out.println("##########################");
+        return "Hello World from psa";
+    }
+}
